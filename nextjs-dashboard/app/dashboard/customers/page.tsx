b4d626213f5c5f6a5f736773372a454172bd1585
mattrang323 <khangnguyenduy74@gmail.com>
2024-09-24 15:31:17 +0700

    adding table in customers page

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import CustomersTable from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const data = await fetchFilteredCustomers(query);
+
+  return (
+      <CustomersTable customers={data} /> 
+  );
 }
