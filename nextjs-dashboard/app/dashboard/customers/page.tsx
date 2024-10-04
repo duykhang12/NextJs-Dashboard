4631abee481117e03bcfa3830ee378954eaf4715
mattrang323 <khangnguyenduy74@gmail.com>
2024-10-04 14:51:56 +0700

    fixing bugs

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -11,12 +11,10 @@ export default async function Page({
 }: {
   searchParams?: {
     query?: string;
-    page?: string;
   };
 }) {
 
   const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 1;
 
   const data = await fetchFilteredCustomers(query);
 
