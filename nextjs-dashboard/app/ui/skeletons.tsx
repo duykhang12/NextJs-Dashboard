f79ecd1955ddd8f6238fdab7054d87d347acef2e
Khang Nguyen <khangnguyenduy74@gmail.com>
2024-10-09 13:41:52 +0700

    Update skeletons.tsx

--- nextjs-dashboard/app/ui/skeletons.tsx
+++ nextjs-dashboard/app/ui/skeletons.tsx
@@ -174,36 +174,28 @@ export function TableSkeleton() {
             <MobileSkeleton />
             <MobileSkeleton />
             <MobileSkeleton />
-            <MobileSkeleton />
           </div>
           <table className="hidden min-w-full text-gray-900 md:table">
             <thead className="rounded-lg text-left text-sm font-normal">
               <tr>
                 <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
-                  Customer
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
-                  Email
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
-                  Amount
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
-                  Date
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
-                  Status
                 </th>
                 <th
                   scope="col"
                   className="relative pb-4 pl-3 pr-6 pt-2 sm:pr-6"
                 >
-                  <span className="sr-only">Action</span>
                 </th>
               </tr>
             </thead>
             <tbody className="bg-white">
-              <TableRowSkeleton />
               <TableRowSkeleton />
               <TableRowSkeleton />
               <TableRowSkeleton />
